fbfd7f8720cb4fcdbdbefc1b9a8aa924d0c93d4d
NikiTesla <krechetov.n@outlook.com>
2024-06-09 18:29:02 +0300

    python bot was deleted

--- model.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-class Model:
-    def __init__(self, model="facebook/blenderbot-400M-distill"):
-        self.chatbot = pipeline(task="conversational", model=model)
-    
-    def discuss(self, prompt: str) -> str:
-        conversation = self.chatbot(prompt)
-        return conversation[-1]["generated_text"]
